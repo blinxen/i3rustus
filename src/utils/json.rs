31e6f9c4788c1e2fad8d5ad06e5bd01f09adfd4f
Blinxen <h-k-81@hotmail.com>
2022-12-09 12:59:45 +0100

    Typo

--- src/utils/json.rs
+++ src/utils/json.rs
@@ -1,7 +1,7 @@
 use serde_json::{ Result, Value };
 
 // AsRef lets us pass any type T that implements AsRef<str>
-// FYI: str and String implent that trait
+// FYI: str and String implement that trait
 pub fn jsonify<T: AsRef<str>>(json_str: &str) -> Result<Value> {
     // as_ref returns the needed &str
     let value: Value = serde_json::from_str(json_str.as_ref())?;

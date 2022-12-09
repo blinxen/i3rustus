97716d584e8e016a51b95c9456dce2e041d639e8
Blinxen <h-k-81@hotmail.com>
2022-12-09 12:59:45 +0100

    Remove old comment

--- src/widgets/time.rs
+++ src/widgets/time.rs
@@ -4,12 +4,6 @@ use chrono::DateTime;
 use crate::widgets::Widget;
 use crate::widgets::WidgetError;
 
-// Why does this "#[derive(Copy, Clone)]" not fix the error:
-// error[E0507]: cannot move out of `**widget` which is behind a shared reference
-//   --> src/main.rs:21:13
-//    |
-// 21 |             widget.display_text();
-//    |             ^^^^^^ move occurs because `**widget` has type `dyn Widget`, which does not implement the `Copy` trait
 pub struct Time { }
 
 impl Widget for Time {

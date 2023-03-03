f9db0c06aada3beca6c3fe7009a7f1100d78efb6
blinxen <h-k-81@hotmail.com>
2023-03-03 16:15:33 +0100

    Implement eth network information

--- src/widgets/network_information.rs
+++ src/widgets/network_information.rs
@@ -7,12 +7,16 @@ use serde_json::Value;
 
 use crate::config::GREEN;
 use crate::config::RED;
+use crate::utils::file::read_first_line_in_file;
 use crate::utils::nm_dbus;
 use crate::widgets::Widget;
 use crate::widgets::WidgetError;
 use crate::LOGGER;
 
 use std::collections::HashMap;
+
+static WIFI_DEVICE_NAME: &str = "wlp3s0";
+static ETH_DEVICE_NAME: &str = "enp5s0";
 
 static ETH_DEFAULT: &str = "E: down";
 static WIFI_DEFAULT: &str = "W: down";
@@ -40,6 +44,8 @@ pub struct NetworkInformation<'a> {
     dbus_connection: Result<Connection, dbus::Error>,
     #[serde(skip_serializing)]
     dbus_wifi_device_path: Result<Path<'static>, dbus::Error>,
+    #[serde(skip_serializing)]
+    dbus_eth_device_path: Result<Path<'static>, dbus::Error>,
 }
 
 impl<'a> NetworkInformation<'a> {
@@ -57,7 +63,18 @@ impl<'a> NetworkInformation<'a> {
                 &Path::new("/org/freedesktop/NetworkManager").unwrap(),
                 "org.freedesktop.NetworkManager",
                 "GetDeviceByIpIface",
-                ("wlp3s0",),
+                (WIFI_DEVICE_NAME,),
+            )
+        } else {
+            Err(dbus::Error::new_failed("Initial dBus connection failed!"))
+        };
+        let dbus_eth_device_path = if let Ok(dbus_connection) = &dbus_connection {
+            nm_dbus::method_call(
+                dbus_connection,
+                &Path::new("/org/freedesktop/NetworkManager").unwrap(),
+                "org.freedesktop.NetworkManager",
+                "GetDeviceByIpIface",
+                (ETH_DEVICE_NAME,),
             )
         } else {
             Err(dbus::Error::new_failed("Initial dBus connection failed!"))
@@ -71,11 +88,62 @@ impl<'a> NetworkInformation<'a> {
             error: None,
             dbus_connection,
             dbus_wifi_device_path,
+            dbus_eth_device_path,
         }
     }
 
     fn get_ethernet_information(&self) -> Result<String, WidgetError> {
-        Ok(ETH_DEFAULT.to_string())
+        let connection: Path = nm_dbus::get_property(
+            self.dbus_connection.as_ref()?,
+            self.dbus_eth_device_path.as_ref()?,
+            "org.freedesktop.NetworkManager.Device",
+            "ActiveConnection",
+        )?;
+
+        if connection.to_string() == "/" {
+            return Ok(ETH_DEFAULT.to_string());
+        }
+
+        let state: u32 = nm_dbus::get_property(
+            self.dbus_connection.as_ref()?,
+            &connection,
+            "org.freedesktop.NetworkManager.Connection.Active",
+            "State",
+        )?;
+
+        // https://people.freedesktop.org/~lkundrak/nm-docs/nm-dbus-types.html#NMActiveConnectionState
+        if state != 1 && state != 2 {
+            return Ok(ETH_DEFAULT.to_string());
+        }
+
+        let mut bitrate: i32 = 0;
+        // bitrate is in kilobits/second
+        // https://www.kernel.org/doc/Documentation/ABI/testing/sysfs-class-net
+        if let Ok(speed) =
+            read_first_line_in_file(&format!("/sys/class/net/{ETH_DEVICE_NAME}/speed"))
+        {
+            bitrate = speed.trim().parse().unwrap();
+        }
+
+        let ip4_config: Path = nm_dbus::get_property(
+            self.dbus_connection.as_ref()?,
+            &connection,
+            "org.freedesktop.NetworkManager.Connection.Active",
+            "Ip4Config",
+        )?;
+
+        let eth_ip: Vec<PropMap> = nm_dbus::get_property(
+            self.dbus_connection.as_ref()?,
+            &ip4_config,
+            "org.freedesktop.NetworkManager.IP4Config",
+            "AddressData",
+        )?;
+
+        Ok(format!(
+            "E: {} Mb/s => {}",
+            bitrate,
+            eth_ip[0].get("address").unwrap().as_str().unwrap(),
+        ))
     }
 
     fn get_wlan_information(&self) -> Result<String, WidgetError> {
@@ -117,8 +185,6 @@ impl<'a> NetworkInformation<'a> {
             "Connection",
         )?;
 
-        // FIXME: When disconnecting and then reconnecting to WIFI then this keeps
-        // return "/" as the object path, which makes no sense
         let ip4_config: Path = nm_dbus::get_property(
             self.dbus_connection.as_ref()?,
             &connection,

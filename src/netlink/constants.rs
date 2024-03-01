f8584be53874d8c9c7bb44a3a17fd57c946b112f
blinxen <h-k-81@hotmail.com>
2024-03-01 23:29:46 +0100

    Remove unused netlink constants

--- src/netlink/constants.rs
+++ src/netlink/constants.rs
@@ -3,16 +3,13 @@ pub const NETLINK_HEADER_SIZE: usize = 16;
 // See https://git.kernel.org/pub/scm/linux/kernel/git/linville/wireless.git/tree/include/uapi/linux/nl80211.h?id=HEAD
 //
 // nl80211_commands
-pub const NL80211_CMD_GET_INTERFACE: i32 = 5;
 pub const NL80211_CMD_GET_STATION: i32 = 17;
 pub const NL80211_CMD_GET_SCAN: i32 = 32;
 
 // nl80211_attrs
 pub const NL80211_ATTR_IFINDEX: i32 = 3;
-pub const NL80211_ATTR_MAC: i32 = 6;
 pub const NL80211_ATTR_STA_INFO: i32 = 21;
 pub const NL80211_ATTR_BSS: i32 = 47;
-pub const NL80211_ATTR_SSID: i32 = 52;
 
 // nl80211_bss
 pub const NL80211_BSS_FREQUENCY: i32 = 2;
